9384f3593a4422363539b8231204e43b130ecb58
Dzhekson6000 <dzhekson6000@gmail.com>
2016-02-07 08:08:44 +0300

    add all

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,12 @@
-#include <stdlib.h> 
+#include <stdlib.h>
+#include "GitHub.h"
+
+using namespace GitHubAPI;
 
 int main(int argc , char *argv[])
 {
+    GitHub gitHub();
+    gitHub.connect();
+    
     return 0;
 }
